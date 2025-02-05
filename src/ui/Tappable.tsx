75b54b3fb9e09bf17853264e5aeac055ff9067c3
Shestoy06 <pochta5550@gmail.com>
2025-02-05 05:52:07 +0100

    .

--- src/ui/Tappable.tsx
+++ src/ui/Tappable.tsx
@@ -15,9 +15,9 @@ export default function Tappable(props: { children: JSX.Element }) {
       class={`rounded-xl transition duration-150 ${
         active() ? "opacity-85 scale-98" : "opacity-100 scale-100"
       }`}
-      /* onPointerDown={() => setActive(true)}
+      onPointerDown={() => setActive(true)}
       onPointerUp={() => setActive(false)}
-      onPointerLeave={() => setActive(false)} */
+      onPointerLeave={() => setActive(false)}
       onClick={handleClick}
     >
       {props.children}
