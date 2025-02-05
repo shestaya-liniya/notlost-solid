f8923ff789fb36a4daf30d84b6d4c65b7ec29ce4
Shestoy06 <pochta5550@gmail.com>
2025-02-05 22:47:21 +0100

    .

--- src/ui/BottomModal.tsx
+++ src/ui/BottomModal.tsx
@@ -11,28 +11,20 @@ const BottomModal = (props: ModalProps) => {
   const { children } = props;
   return (
     <div
-      class={`fixed h-screen top-0 left-0 inset-0 flex items-center justify-center z-50 transition-all duration-300 ease ${
+      class={`bg-primary pointer-events-auto p-6 rounded-t-2xl shadow-lg transition-all ease-in-out duration-300 absolute bottom-0 w-full ${
         props.isOpen
-          ? "pointer-events-none opacity-100"
-          : "pointer-events-none opacity-0"
+          ? " translate-y-0 opacity-100"
+          : " translate-y-full opacity-0"
       }`}
+      onClick={(e) => e.stopPropagation()}
     >
+      <div class="text-2xl font-semibold text-center mb-4">{props.title}</div>
+      {children}
       <div
-        class={`bg-primary pointer-events-auto p-6 rounded-t-2xl shadow-lg transition-all ease-in-out duration-300 absolute bottom-0 w-full ${
-          props.isOpen
-            ? " translate-y-0 opacity-100"
-            : " translate-y-full opacity-0"
-        }`}
-        onClick={(e) => e.stopPropagation()}
+        class="absolute top-2 right-4 text-xl font-semibold text-link"
+        onClick={props.onClose}
       >
-        <div class="text-2xl font-semibold text-center mb-4">{props.title}</div>
-        {children}
-        <div
-          class="absolute top-2 right-4 text-xl font-semibold text-link"
-          onClick={props.onClose}
-        >
-          Done
-        </div>
+        Done
       </div>
     </div>
   );
