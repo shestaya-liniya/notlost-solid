70c332a6a2dd944a1547d2aaaf6b02f2b55f0fd4
Shestoy06 <pochta5550@gmail.com>
2025-02-05 15:23:01 +0100

    .

--- src/ui/BottomModal.tsx
+++ src/ui/BottomModal.tsx
@@ -11,23 +11,23 @@ const BottomModal = (props: ModalProps) => {
   return (
     <div
       class={`fixed inset-0 flex items-center justify-center z-50 transition-opacity duration-300 ${
-        props.isOpen ? "opacity-100" : "opacity-100 pointer-events-none"
+        props.isOpen ? "pointer-events-none" : "pointer-events-none"
       }`}
     >
       <div
-        class={`bg-primary p-6 rounded-t-2xl shadow-lg transform transition-transform ease-in-out duration-300 absolute bottom-0 w-full ${
+        class={`bg-primary pointer-events-auto p-6 rounded-t-2xl shadow-lg transform transition-transform ease-in-out duration-300 absolute bottom-0 w-full ${
           props.isOpen ? "translate-y-0" : "translate-y-full"
         }`}
         onClick={(e) => e.stopPropagation()}
       >
         <div class="text-2xl font-semibold text-center mb-4">{props.title}</div>
         {props.children}
-        <button
-          class="absolute top-4 right-4 text-link font-bold"
+        <div
+          class="absolute top-2 right-4 text-xl font-semibold text-link"
           onClick={props.onClose}
         >
           Done
-        </button>
+        </div>
       </div>
     </div>
   );
