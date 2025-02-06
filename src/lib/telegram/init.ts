f412b39fe6d03f42fecdee8211d1a395268a68e9
Shestoy06 <pochta5550@gmail.com>
2025-02-06 13:31:33 +0100

    try

--- src/lib/telegram/init.ts
+++ src/lib/telegram/init.ts
@@ -8,7 +8,6 @@ import {
   init as initSDK,
   postEvent,
 } from "@telegram-apps/sdk-solid";
-import WebApp from "@twa-dev/sdk";
 
 /**
  * Initializes the application and configures its dependencies.
@@ -35,9 +34,10 @@ export function init(debug: boolean): void {
   miniApp.bindCssVars();
   themeParams.bindCssVars();
 
-  if (WebApp.default) {
-    WebApp.default.requestFullscreen();
-    WebApp.default.disableVerticalSwipes();
+  const webApp = (window as any)?.Telegram?.WebApp;
+  if (webApp) {
+    webApp.requestFullscreen();
+    webApp.disableVerticalSwipes();
   } else {
     postEvent("web_app_setup_swipe_behavior", {
       allow_vertical_swipe: false,
